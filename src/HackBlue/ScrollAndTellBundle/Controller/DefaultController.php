b08fca742f63dd13298832121dc155b636f84247
Keith Morris <keith.a.morris@gmail.com>
2013-04-07 03:30:15 -0400

    Added start page

--- src/HackBlue/ScrollAndTellBundle/Controller/DefaultController.php
+++ src/HackBlue/ScrollAndTellBundle/Controller/DefaultController.php
@@ -3,11 +3,28 @@
 namespace HackBlue\ScrollAndTellBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use HackBlue\ScrollAndTellBundle\Document\Story;
+
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('HackBlueScrollAndTellBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function startAction()
+    {
+    	$story = new Story();
+        $story->setName('My New Scrollorama');
+
+        $form = $this->createFormBuilder($story)
+            ->add('name', 'text')
+            ->getForm();
+
+        return $this->render('HackBlueScrollAndTellBundle:Story:start.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
